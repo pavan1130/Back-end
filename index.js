35624239ecf995e6e0c72569bf26f6c30a52e5f9
pavan/1130 <bpavankumar516@gmail.com>
2023-10-26 10:35:13 +0530

    rr

--- index.js
+++ index.js
@@ -5,14 +5,24 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 
+// Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI, {
+// MongoDB Connection
+const mongodbURI = process.env.MONGODB_URI; // Ensure this environment variable is correctly set
+
+if (!mongodbURI) {
+  console.error("MONGODB_URI environment variable is not set.");
+  process.exit(1);
+}
+
+mongoose.connect(mongodbURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 const db = mongoose.connection;
 
 db.on("error", (error) => {
@@ -23,12 +33,13 @@ db.once("open", () => {
   console.log("Connected to MongoDB");
 });
 
-db.once("close", () => {
-  console.log("Disconnected from MongoDB");
-});
+// Routes
 app.use("/api/users", require("./routes/authRoutes"));
-app.use("/api/users", require("./routes/taskRoutes"));
+app.use("/api/tasks", require("./routes/taskRoutes")); // Updated route path for tasks
 app.use("/api", require("./routes/emailRoutes"));
 
+// Server Configuration and Startup
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
